b227631cc7e99ebb77dad665ea0bb727c5e08325
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-21 17:13:37 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,5 @@ def adding_to_dennis
         }
      }
 		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-		 programmer_hash
+		 programmer_hash[:dennis_ritchie][:languages][0]
 end
