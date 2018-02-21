5a050fed29b1101960162606b113ef2a0c7102db
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-21 17:10:58 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,6 @@ return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 
 	programmer_hash =
  		{
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -65,7 +66,10 @@ def adding_matz
 # }
 
 	programmer_hash =
- 		{
+ 		{ 	 :yukihiro_matsumoto => {
+		  	:known_for => "Ruby",
+		   	:languages => ["LISP", "C"]},
+
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
